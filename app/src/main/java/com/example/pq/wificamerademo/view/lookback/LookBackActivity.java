c8c4c2f55b435e50910c47c1f2479731000fddf2
RedColinar <1126525692@qq.com>
2018-08-08 13:30:21 +0800

    'lookback'

--- app/src/main/java/com/example/pq/wificamerademo/view/lookback/LookBackActivity.java
+++ app/src/main/java/com/example/pq/wificamerademo/view/lookback/LookBackActivity.java
@@ -13,6 +13,7 @@ import com.example.pq.wificamerademo.rx.BaseObserver;
 import com.example.pq.wificamerademo.util.StorageUtil;
 import com.google.vr.sdk.widgets.pano.VrPanoramaView;
 import com.icatch.wificam.customer.type.ICatchFile;
+import com.icatch.wificam.customer.type.ICatchFrameBuffer;
 
 import java.io.File;
 
@@ -56,22 +57,22 @@ public class LookBackActivity extends AppCompatActivity {
                 final String filePath = path + fileAdded.getFileHandle() + ".jpg";
 
                 // 抛出异常的地方
-                CameraFile.getInstance().downloadFile(fileAdded, filePath);
-
-                panoView.loadImageFromBitmap(BitmapFactory.decodeFile(filePath), panoOptions);
+//                CameraFile.getInstance().downloadFile(fileAdded, filePath);
+//
+//                panoView.loadImageFromBitmap(BitmapFactory.decodeFile(filePath), panoOptions);
 
                 // 如果用下面的 ICatchFrameBuffer 可以成功看照片的回放，但是退出当前页面，回到 PreviewActivity 后无法再次进入预览
-//                ICatchFrameBuffer buffer = CameraFile.getInstance().downloadFile(fileAdded);
-//                if (buffer != null) {
-//                    panoOptions.inputType = VrPanoramaView.Options.TYPE_MONO;
-//                    try {
-//                        // panoView.loadImageFromByteArray(buffer.getBuffer(), panoOptions);
-//                        panoView.loadImageFromBitmap(BitmapFactory.decodeByteArray(buffer.getBuffer(), 0, buffer.getBuffer().length), panoOptions);
-//                    } catch (Exception e) {
-//                        e.printStackTrace();
-//                    }
-//                }
-//                return buffer;
+                ICatchFrameBuffer buffer = CameraFile.getInstance().downloadFile(fileAdded);
+                if (buffer != null) {
+                    panoOptions.inputType = VrPanoramaView.Options.TYPE_MONO;
+                    try {
+                        // panoView.loadImageFromByteArray(buffer.getBuffer(), panoOptions);
+                        panoView.loadImageFromBitmap(BitmapFactory.decodeByteArray(buffer.getBuffer(), 0, buffer.getBuffer().length), panoOptions);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+                return buffer;
             }
             return null;
         })
